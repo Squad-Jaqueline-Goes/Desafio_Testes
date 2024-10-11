103f1833471c2989f0b4b533491ebecd48962708
ludmylla <ludmyllacaetano1@gmail.com>
2024-10-11 16:29:53 -0300

    Adicionando os testes

---
+++ test_exercise.py
@@ -0,0 +1,34 @@
+import pytest
+from desafio2 import admin_command
+
+class TestAdminCommand:
+
+    def command(self):
+        return ["ps", "aux"]
+
+    def test_no_sudo(self):
+        result = admin_command(self.command(), sudo=False)
+        assert result == self.command()
+
+    def test_sudo(self):
+        result = admin_command(self.command(), sudo=True)
+        expected = ["sudo"] + self.command()
+        assert result == expected
+    
+    def test_non_list_commands(self):
+        with pytest.raises(TypeError) as error:
+            admin_command("some command", sudo=True)
+        assert error.value.args[0] == "was expecting command to be a list, but got a <class 'str'>"
+
+    def test_empty_command(self):
+        result = admin_command([], sudo=True)
+        assert result == ["sudo"]
+
+    def test_no_sudo_empty_command(self):
+        result = admin_command([], sudo=False)
+        assert result == []
+
+    def test_complex_command(self):
+        command = ["curl", "-I", "http://example.com"]
+        result = admin_command(command, sudo=True)
+        assert result == ["sudo"] + command
