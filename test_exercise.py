e3d73fe323a91c8e0cf4da919b2fa1dd9320b3c0
Fernanda Barbosa <112911182+NandayGB@users.noreply.github.com>
2024-10-11 16:31:58 -0300

    adicionando_novos_testes.py

--- test_exercise.py
+++ test_exercise.py
@@ -32,3 +32,18 @@ class TestAdminCommand:
         command = ["curl", "-I", "http://example.com"]
         result = admin_command(command, sudo=True)
         assert result == ["sudo"] + command
+
+    def test_complex_command_no_sudo(self):
+        command = ["curl", "-I", "http://example.com/"]
+        result = admin_command(command, sudo=False)
+        assert result == command
+
+    def test_invalid_command(self):
+        with pytest.raises(TypeError) as error:
+            admin_command(123, sudo=True)
+        assert error.value.args[0] == "was expecting command to be a list, but got a <class 'int'>"
+
+    def test_empty_string_command(self):
+        with pytest.raises(TypeError) as error:
+            admin_command("", sudo=True)
+        assert error.value.args[0] == "was expecting command to be a list, but got a <class 'str'>"
